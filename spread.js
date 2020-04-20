23f7c1bb8f3e2ad1e4176a155e14acb655d25ee4
umachaitanya <umachaitanya3@gmail.com>
2020-04-20 04:44:16 -0400

    first commit

---
+++ spread.js
@@ -0,0 +1,13 @@
+
+const str = "abcd";
+console.log(...str);
+console.log([...str]);
+
+const arr=[1,2,3];
+console.log(...arr);
+console.log([...arr]);
+
+function result(...res){
+    console.log(result)
+};
+result(4,5,6);
